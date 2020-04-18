aa494e9e27589fcd2f420a2e0ee90996221b4ee4
Pasho <pfedulov84@gmail.com>
2020-04-18 20:56:53 +0100

    Stave, StaveLine and BarLine

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import { settings } from 'cluster';
 
 const range = (length: number) => Array.from(Array(length).keys());
 
@@ -9,27 +8,36 @@ const Settings = {
   height: 600,
   padding: 50,
   lineSpacing: 10,
-  staveHeight: () => Settings.lineSpacing * 5,
-  staveHeightWithPadding: () => Settings.staveHeight() + Settings.padding
+  staveHeight: () => Settings.lineSpacing * 4,
+  staveHeightWithPadding: () => Settings.staveHeight() + Settings.padding,
+  staveWidth: () => Settings.width - Settings.padding * 2,
+  barsPerStave: 7,
+  barWidth: () => Settings.staveWidth() / Settings.barsPerStave
 }
 
-const Line = (props: {  
+const BarLine = (props: {
+  x: number;
+  y: number;
+}) => <line x1={props.x} y1={props.y} x2={props.x} y2={props.y + Settings.staveHeight()} className="tab-line" />
+
+const StaveLine = (props: {
   y: number
 }) => <line x1={Settings.padding} y1={props.y} x2={Settings.width - Settings.padding} y2={props.y} className="tab-line" />
+
+const Stave = (props: {
+  y: number
+}) => (
+    <>      
+      {range(5).map(i => <StaveLine key={i} y={props.y + i * Settings.lineSpacing} />)}
+      {range(Settings.barsPerStave + 1).map(i => <BarLine key={i} y={props.y} x={Settings.padding + i * Settings.barWidth()} />)}      
+    </>
+  )
 
 function App() {
   return (
     <div className="App">
       <svg height={Settings.height} width={Settings.width}>
-        {range(5).map(i => {
-          return range(5).map(j => {
-            const y = Settings.padding + Settings.staveHeightWithPadding() * i + j * Settings.lineSpacing;                   
-            return (
-              <Line key={j} y={y} />
-            )
-          })
-        })}
-        
+        {range(5).map(i => <Stave key={i} y={Settings.padding + Settings.staveHeightWithPadding() * i} />)}
       </svg>
     </div>
   );
