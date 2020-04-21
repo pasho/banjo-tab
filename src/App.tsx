128da81a3f36fe927d9c41d7a03c52d7cccf828c
Pavel <pfedulov84@gmail.com>
2020-04-21 23:31:58 +0100

    react router for basic navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import { Switch, Route, Redirect, BrowserRouter as Router, Link } from 'react-router-dom';
 import HopHighLadies from "./tunes/HopHighLadies";
 import WorriedMansBlues from "./tunes/WorriedMansBlues";
 import './App.css';
 
+
 const App = () => {
   return (
-    <div className="App">
-      <HopHighLadies />
-      <WorriedMansBlues />
-    </div>
+    <Router>
+      <div className="App">
+        <span>
+          <Link to="/worried-man">Worried Man's Blues</Link>
+          ::
+          <Link to="/hop-high-ladies">Hop High Ladies</Link>
+        </span>
+        <Switch>
+          <Route path="/worried-man">
+            <WorriedMansBlues />
+          </Route>
+          <Route path="/hop-high-ladies">
+            <HopHighLadies />
+          </Route>
+          <Route path="/">
+            <Redirect to="/worried-man"/>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
