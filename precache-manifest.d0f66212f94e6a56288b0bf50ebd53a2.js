533db95236cff58f6f9822332e2fdfc222f9ed9e
Pavel <pfedulov84@gmail.com>
2020-04-22 19:40:06 +0100

    Updates

---
+++ precache-manifest.d0f66212f94e6a56288b0bf50ebd53a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b75eaf2eb51c54dd06fec5884574a44",
+    "url": "/banjo-tab/index.html"
+  },
+  {
+    "revision": "5fe3474f14fa23039a92",
+    "url": "/banjo-tab/static/css/main.6ffa391c.chunk.css"
+  },
+  {
+    "revision": "4486145dc67e8d2aba4b",
+    "url": "/banjo-tab/static/js/2.21d982a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/banjo-tab/static/js/2.21d982a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fe3474f14fa23039a92",
+    "url": "/banjo-tab/static/js/main.0b46d5a8.chunk.js"
+  },
+  {
+    "revision": "1d4966c23f02d72c5a12",
+    "url": "/banjo-tab/static/js/runtime-main.e9dd6d57.js"
+  }
+]);
