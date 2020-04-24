97264955ebc71d60fbfaa15af11f9e5f90104a96
Pavel <pfedulov84@gmail.com>
2020-04-24 23:12:47 +0100

    Updates

--- precache-manifest.afef94a9a2e927e0cd3bd53e200fac8e.js
+++ precache-manifest.afef94a9a2e927e0cd3bd53e200fac8e.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "459dd0e9d646c1658b034f6d6e0f331f",
+    "revision": "1a9600bb221b3d1692056ad5d269349c",
     "url": "/banjo-tab/index.html"
   },
   {
-    "revision": "5828ea35c3ef11480221",
+    "revision": "d4c6d155fc738e0c7f00",
     "url": "/banjo-tab/static/css/main.6ffa391c.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/banjo-tab/static/js/2.809ec4fc.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5828ea35c3ef11480221",
-    "url": "/banjo-tab/static/js/main.43e4190a.chunk.js"
+    "revision": "d4c6d155fc738e0c7f00",
+    "url": "/banjo-tab/static/js/main.d7fff62b.chunk.js"
   },
   {
     "revision": "1d4966c23f02d72c5a12",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/banjo-tab/precache-manifest.72ba27af2b06f18c7dbb707a796cf711.js"
+  "/banjo-tab/precache-manifest.afef94a9a2e927e0cd3bd53e200fac8e.js"
 );
 
 self.addEventListener('message', (event) => {
