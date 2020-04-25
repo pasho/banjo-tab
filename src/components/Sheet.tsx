75be7075ba27597bb42e362033cfd17b9f26b224
Pavel <pfedulov84@gmail.com>
2020-04-25 19:09:48 +0100

    Moved all editing logic to editor

--- src/components/Sheet.tsx
+++ src/components/Sheet.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as Settings from "../settings";
 import { Stave } from "./Stave";
 import { useStyle } from "./StyleProvider";
-import { useContext, useState } from "react";
+import { useContext } from "react";
 
 type SheetState = {
   tuning: string;
@@ -18,24 +18,6 @@ const SheetContext = React.createContext<SheetState>(defaultState);
 
 export const useSheet = () => useContext(SheetContext);
 
-type Editor = {
-  position: number;
-  addNote: (note: string) => void;
-  move: (direction: "left" | "right" | "up" | "down") => void;
-  del: () => void;
-  backspace: () => void;
-}
-
-const EditorContext = React.createContext<Editor>({
-  position: 0,
-  addNote: _ => { },
-  move: _ => { },
-  del: () => { },
-  backspace: () => { }
-});
-
-export const useEditor = () => useContext(EditorContext);
-
 type SheetProps = {
   title: string;
   tuning?: string;
@@ -46,56 +28,12 @@ type SheetProps = {
 export const Sheet: React.FunctionComponent<SheetProps> = (props) => {
   const { tuning, meter, title, children } = props;
   const { barsPerStave } = useStyle();
-  const [notes, setNotes] = useState(props.notes.split(";").map(s => s.trim()));
-  const [position, setPosition] = useState(0);
-  const maxPosition = notes.length;
+  const notes = props.notes.split(";").map(s => s.trim());
 
   const sheetContext = {
     tuning: tuning ?? defaultState.tuning,
-    meter: meter ?? defaultState.meter,
-    notes,
-    setNotes
+    meter: meter ?? defaultState.meter
   };
-  const editorContext: Editor = {
-    position,
-    move: (direction) => {
-      switch (direction) {
-        case "left":
-          setPosition(Math.max(0, position - 1));
-          break;
-        case "right":
-          setPosition(Math.min(maxPosition, position + 1));
-          break;
-      }
-    },
-    addNote: (note) => {
-      setNotes(notes =>
-        [
-          ...notes.slice(0, position),
-          note,
-          ...notes.slice(position)
-        ]
-      );
-      setPosition(Math.min(maxPosition, position + 1));
-    },
-    del: () => {
-      if (position < notes.length) {
-        setNotes(notes =>
-          [
-            ...notes.slice(0, position),
-            ...notes.slice(position + 1)
-          ]);
-      }
-    },
-    backspace: () => {
-      setNotes(notes =>
-        [
-          ...notes.slice(0, position - 1),
-          ...notes.slice(position)
-        ]);
-      setPosition(Math.max(0, position - 1));
-    }
-  }
   const staveBarNotes = notes
     .reduce(
       (acc: string[][][], note, noteIndex) => {
@@ -122,20 +60,18 @@ export const Sheet: React.FunctionComponent<SheetProps> = (props) => {
 
   return (
     <SheetContext.Provider value={sheetContext} >
-      <EditorContext.Provider value={editorContext}>
-        <h1>{title}</h1>
-        <p>{tuning}</p>
-        <svg viewBox={`0 0 ${Settings.width} ${sheetHeight}`} preserveAspectRatio="xMidYMid meet" style={{ maxWidth: `${Settings.width}px` }}>
-          {staveBarNotes.map(
-            (barNotes, staveIndex) => {
-              return (
-                <Stave key={staveIndex} y={.5 * Settings.padding() + Settings.staveHeightWithPadding() * staveIndex} barNotes={barNotes} />
-              )
-            }
-          )}
-          {children}
-        </svg>
-      </EditorContext.Provider>
+      <h1>{title}</h1>
+      <p>{tuning}</p>
+      <svg viewBox={`0 0 ${Settings.width} ${sheetHeight}`} preserveAspectRatio="xMidYMid meet" style={{ maxWidth: `${Settings.width}px` }}>
+        {staveBarNotes.map(
+          (barNotes, staveIndex) => {
+            return (
+              <Stave key={staveIndex} y={.5 * Settings.padding() + Settings.staveHeightWithPadding() * staveIndex} barNotes={barNotes} />
+            )
+          }
+        )}
+        {children}
+      </svg>
     </SheetContext.Provider >
   )
 
--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,11 +1,81 @@
-import React, { useEffect, useCallback } from 'react';
-import { Sheet, useSheet, useEditor } from '../components/Sheet';
+import React, { useEffect, useCallback, useState, useContext, useMemo } from 'react';
+import { Sheet, useSheet } from '../components/Sheet';
 import * as Settings from "../settings";
 import { useStyle } from '../components/StyleProvider';
 
+const EditorContext = React.createContext<{
+  position: number;
+}>({
+  position: 0
+});
+
+export const useEditor = () => useContext(EditorContext);
+
 const Cursor = () => {
   const { meter } = useSheet();
-  const { addNote, move, position, backspace, del } = useEditor();
+  const { position } = useEditor();
+  const { barsPerStave } = useStyle();
+  const noteWidth = Settings.staveWidth() / barsPerStave / meter;
+  const x = Settings.padding() + (.1 + position) * noteWidth;
+  const height = Settings.lineSpacing * 5;
+  const y1 = .5 * Settings.padding() - .5 * Settings.lineSpacing;
+  const y2 = y1 + height;
+
+  return (
+    <line
+      x1={x} y1={y1} x2={x} y2={y2}
+      strokeWidth={1} stroke="black">
+      <animate attributeName="visibility" from="visible" to="hidden" dur="1s" repeatCount="indefinite" />
+    </line>
+  )
+}
+
+export default () => {
+  const [notes, setNotes] = useState<string[]>([]);
+  const [position, setPosition] = useState(0);
+
+  const { move, addNote, del, backspace } = useMemo(() => {
+    return {
+      move: (direction: "left" | "right") => {
+        switch (direction) { 
+          case "left":
+            setPosition(Math.max(0, position - 1));
+            break;
+          case "right":
+            setPosition(Math.min(notes.length, position + 1));
+            break;
+        }
+      },
+      addNote: (note: string) => {
+        setNotes(
+          [
+            ...notes.slice(0, position),
+            note,
+            ...notes.slice(position)
+          ]
+        );
+        setPosition(position + 1);
+      },
+      del: () => {
+        if (position < notes.length) {
+          setNotes(
+            [
+              ...notes.slice(0, position),
+              ...notes.slice(position + 1)
+            ]);
+        }
+      },
+      backspace: () => {
+        setNotes(
+          [
+            ...notes.slice(0, Math.max(0, position - 1)),
+            ...notes.slice(position)
+          ]);
+        setPosition(Math.max(0, position - 1));
+      }
+    }
+  }, [position, setPosition, notes, setNotes]);
+
   const onKey = useCallback(({ keyCode }: KeyboardEvent) => {
     switch (keyCode) {
       case 37:
@@ -14,7 +84,7 @@ const Cursor = () => {
       case 39:
         move("right");
         break;
-      case 8: 
+      case 8:
         backspace();
         break;
       case 46:
@@ -30,31 +100,13 @@ const Cursor = () => {
     document.addEventListener("keydown", onKey);
     return () => document.removeEventListener("keydown", onKey);
   }, [onKey]);
-  
-
-  const { barsPerStave } = useStyle();
-  const noteWidth = Settings.width / barsPerStave / meter;
-  const x = Settings.padding() + (.1 + position) * noteWidth;
-  const height = Settings.lineSpacing * 5;
-  const y1 = .5 * Settings.padding() - .5 * Settings.lineSpacing;
-  const y2 = y1 + height;
 
   return (
-    <line 
-      x1={x} y1={y1} x2={x} y2={y2} 
-      strokeWidth={1} stroke="black">
-      <animate attributeName="visibility" from="visible" to="hidden" dur="1s" repeatCount="indefinite" />
-    </line>
-  )
-}
-
-export default () => {
-  return (
-    <>
-      <Sheet title="Editor" notes="">
-        <Cursor/>
+    <EditorContext.Provider value={{position}}>
+      <Sheet title="Editor" notes={notes.join(";")}>
+        <Cursor />
       </Sheet>
-    </>
+    </EditorContext.Provider>
   )
 
 }
