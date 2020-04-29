285ae657bc01994285f9da436d637e28c986d436
Pavel <pfedulov84@gmail.com>
2020-04-29 22:37:43 +0100

    Prototyping virtual sheet

--- src/text-editor/Editor.tsx
+++ src/text-editor/Editor.tsx
@@ -3,6 +3,7 @@ import { Sheet } from "../components/Sheet";
 import { useState, useContext } from "react";
 import Cursor from "./Cursor";
 import Settings from "../components/Settings";
+import { range } from "../utils";
 
 const EditorContext = React.createContext<{
   position: number;
@@ -14,17 +15,42 @@ const EditorContext = React.createContext<{
 
 export const useTextEditor = () => useContext(EditorContext);
 
+const VirtualSheet: React.FunctionComponent<{
+  notes: string;
+  title: string;
+}> = ({ notes, title, children }) => {
+  const notesArray = notes.split(";");
+  const visibleNotes = notesArray.slice(
+    Math.max(0, notesArray.length - 8),
+    notesArray.length
+  );
+  const blanks = range(Math.max(0, 8 - notesArray.length)).map((_) => "");
+  const visibleNotesWithBlanks = [...visibleNotes, ...blanks].join(";");
+  console.log(visibleNotesWithBlanks);
+  return (
+    <Sheet
+      {...{ title, notes: visibleNotesWithBlanks }}
+      notes={visibleNotesWithBlanks}
+    >
+      {children}
+    </Sheet>
+  );
+};
+
 export default () => {
   const [notes, setNotes] = useState("");
   const [textPosition, setTextPosition] = useState(0);
-  const position = notes.substr(0, textPosition).split(";").length - 1;
+  const truePosition = notes.substr(0, textPosition).split(";").length - 1;
+  const notesCount = notes.split(";").length;
+  const positionAdjustment = Math.max(0, notesCount - 8);
+  const position = truePosition - positionAdjustment;
   const meter = 4;
   return (
     <EditorContext.Provider value={{ position, meter }}>
       <Settings {...{ sidePaddingEnabled: false, width: 400, barsPerStave: 2 }}>
-        <Sheet title="Editor" notes={notes}>
+        <VirtualSheet title="Editor" notes={notes}>
           <Cursor />
-        </Sheet>
+        </VirtualSheet>
       </Settings>
       <br />
       <textarea
