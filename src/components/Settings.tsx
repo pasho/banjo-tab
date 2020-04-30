1c658a932db33554db35da26ff5733443dfbdea2
Pavel <pfedulov84@gmail.com>
2020-04-30 22:00:49 +0100

    Restruct of settings

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -2,71 +2,104 @@ import * as React from "react";
 import { useContext, useMemo } from "react";
 import { merge } from "../utils";
 
+const width = 800;
+const lineSpacing = 10;
+const sidePaddingEnabled = true;
+const barsPerStave = 4;
+// Accounts for font size and svg discrepancies.
+const textCharCentreOffset = {
+  x: -3.5,
+  y: 4,
+};
+// Accounts for different widths of vertical and horizontal lines and ugly edges.
+const noteHorizontalLineAdjustment = 0.5;
+const showNotes = false;
+
 const coreSettings = {
-  width: 800,
-  lineSpacing: 10,
-  sidePaddingEnabled: true,
-  barsPerStave: 4,
-  // Accounts for font size and svg discrepancies.
-  textCharCentreOffset: {
-    x: -3.5,
-    y: 4,
-  },
-  // Accounts for different widths of vertical and horizontal lines and ugly edges.
-  noteHorizontalLineAdjustment: 0.5,
-  showNotes: false,
+  width,
+  lineSpacing,
+  sidePaddingEnabled,
+  barsPerStave,
+  textCharCentreOffset,
+  noteHorizontalLineAdjustment,
+  showNotes,
 };
 
 const CoreSettings = React.createContext(coreSettings);
 
 const useCoreSettings = () => useContext(CoreSettings);
 
-const getDerivedSettings = ({
-  width,
-  lineSpacing,
-  sidePaddingEnabled,
-}: Pick<
-  typeof coreSettings,
-  "width" | "lineSpacing" | "sidePaddingEnabled"
->) => {
-  const padding = 5 * lineSpacing;
-  const staveHeight = lineSpacing * 4;
-  const sidePadding = sidePaddingEnabled ? padding : 0;
-  return {
-    padding,
-    staveHeight,
-    sidePadding,
-    staveHeightWithPadding: staveHeight + padding,
-    staveWidth: width - sidePadding * 2,
-  };
-};
+const getPadding = (lineSpacing: number) => 5 * lineSpacing;
+const getStaveHeight = (lineSpacing: number) => lineSpacing * 4;
+const getSidePadding = (sidePaddingEnabled: boolean, padding: number) =>
+  sidePaddingEnabled ? padding : 0;
+const getStaveHeightWithPadding = (staveHeight: number, padding: number) =>
+  staveHeight + padding;
+const getStaveWidth = (width: number, sidePadding: number) =>
+  width - sidePadding;
+const getBarWidth = (staveWidth: number, barsPerStave: number) =>
+  staveWidth / barsPerStave;
 
-const SettingsContext = React.createContext({
-  ...coreSettings,
-  ...getDerivedSettings(coreSettings),
+const padding = getPadding(coreSettings.lineSpacing);
+const staveHeight = getStaveHeight(coreSettings.lineSpacing);
+const sidePadding = getSidePadding(coreSettings.sidePaddingEnabled, padding);
+
+const staveHeightWithPadding = staveHeight + padding;
+const staveWidth = coreSettings.width - sidePadding * 2;
+const barWidth = getBarWidth(staveWidth, coreSettings.barsPerStave);
+
+const DerivedSettingsContext = React.createContext({
+  padding,
+  staveHeight,
+  sidePadding,
+  staveHeightWithPadding,
+  staveWidth,
+  barWidth,
 });
 
-export const useSettings = () => useContext(SettingsContext);
+export const useSettings = () => ({
+  ...useContext(CoreSettings),
+  ...useContext(DerivedSettingsContext),
+});
 
 const Settings: React.FunctionComponent<Partial<typeof coreSettings>> = (
   props
 ) => {
-  const mergedCoreSettings = merge(useCoreSettings(), props);
+  const coreSettings = merge(useCoreSettings(), props);
+  const { lineSpacing, sidePaddingEnabled, width, barsPerStave } = coreSettings;
 
-  const derivedSettings = useMemo(
-    () => getDerivedSettings(mergedCoreSettings),
-    [mergedCoreSettings]
+  const padding = useMemo(() => getPadding(lineSpacing), [lineSpacing]);
+  const staveHeight = useMemo(() => getStaveHeight(lineSpacing), [lineSpacing]);
+  const sidePadding = useMemo(
+    () => getSidePadding(sidePaddingEnabled, lineSpacing),
+    [sidePaddingEnabled, lineSpacing]
   );
-
-  const settings = {
-    ...mergedCoreSettings,
-    ...derivedSettings,
-  };
+  const staveHeightWithPadding = useMemo(
+    () => getStaveHeightWithPadding(staveHeight, padding),
+    [staveHeight, padding]
+  );
+  const staveWidth = useMemo(() => getStaveWidth(width, sidePadding), [
+    width,
+    sidePadding,
+  ]);
+  const barWidth = useMemo(() => getBarWidth(staveWidth, barsPerStave), [
+    staveWidth,
+    barsPerStave,
+  ]);
   return (
-    <CoreSettings.Provider value={mergedCoreSettings}>
-      <SettingsContext.Provider value={settings}>
+    <CoreSettings.Provider value={coreSettings}>
+      <DerivedSettingsContext.Provider
+        value={{
+          padding,
+          staveHeight,
+          sidePadding,
+          staveHeightWithPadding,
+          staveWidth,
+          barWidth,
+        }}
+      >
         {props.children}
-      </SettingsContext.Provider>
+      </DerivedSettingsContext.Provider>
     </CoreSettings.Provider>
   );
 };
