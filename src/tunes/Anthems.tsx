724a86d3ad5fe3b87c18bdf7448ce0c81691929e
Pavel <pfedulov84@gmail.com>
2020-06-10 20:16:07 +0100

    Added version with chords

--- src/tunes/Anthems.tsx
+++ src/tunes/Anthems.tsx
@@ -50,25 +50,81 @@ export const RussianFull = () => (
       
       C:m 1;d 0,  2;m  0;d    x,  0;
 
-      b 1;h  2;b 0;d   4,   4;
-      b  2;d  0,   5;b  0;d   0,   0;
-      b   2;s   2,   4;b   5;s   5,   7;
-      b  2;h 1;m0;d    x,  0;
-      b2;d0, 1;b0;d 0,  0;
-      b 1;d 0,  2;b 0;d   4,   4;
-      b  2;d  0,   5;b  0;d   0,   0;
-      b 1; d 0,  2;h  4;h 1, 3;
-      m2;;d0, 1;h 1;
-      m0;d    x,  0;h  4;h 1, 3;
-      m 1;;d 0,  2;h  2;    
-      m 0;d    x,   4;s   4,   7;s  2,  4;
-      m 1;s  2,  4;m 1;s  2,  4;
-      m 1;s  2,  5;m3;d3,    x;
-      m3;;p2;p 1;
-      m2;d    x, 1;m 1;;
-      m0;;p 1;p  2;
-      m 1;d    x,  2;m  2;;
-      m 1;d 0,  2;m  0;d   0,   0;
+      C:b 1;h  2;G:b 0;d   4,   4;
+      Am:b  2;d  0,   5;C:b  0;d   0,   0;
+      Dm:b   2;s   2,   4;F:b   5;p   5,  0;
+      Am:b  2;h 1;G:m0;d    x,  0;
+      C:b2;d0, 1;G:b0;d 0,  0;
+      Am:b 1;d 0,  2;Em:b 0;d   4,   4;
+      F:b  2;d  0,   5;C:b  0;d   0,   0;
+      C:b 1; d 0,  2;G:p  0, 0;h 1, 3;
+      C:m2;;d0, 1;h 1;
+      G:m0;d    x,  0;p  0, 0;h 1, 3;
+      C:m 1;;d 0,  2;h  2;    
+      G:m 0;d    x,   4;p   4,  0;p  2, 0;
+      C:m 1;p  2, 0;m 1;p  2, 0;
+      m 1;p  2, 1;F:m3;d3,    x;
+      F:m3;;G:p2;p 1;
+      C:m2;d    x, 1;m 1;;
+      Dm:m0;;p 1;p  2;
+      Am:m 1;d    x,  2;m  2;;
+      C:m 1;d 0,  2;m  0;d   0,   0;
+
+      m  0;;m  2;m 0;
+      m 1;
+    `}
+  />
+);
+
+export const RussianFullWithChords = () => (
+  <Sheet
+    title="Russian National Anthem (with chords)"
+    tuning="gCGBd"
+    barsPerStave={5}
+    notes={`
+      C:m2100;;;d    x,  0;
+      
+      C:b 1;h  2;G:b 0;d   4,   4;
+      Am:b  2;d  0,   5;C:b  0;d   0,   0;
+      Dm:b   2;s   2,   4;F:b   5;p   5,  0;
+      Am:b  2;h 1;G:b0002;d    x,  0;
+      C:b2;d0, 1;G:b0;d 0,  0;
+      Am:b 1;d 0,  2;Em:b 0;d   4,   4;
+      F:b  2;d  0,   5;C:b  0;d   0,   0;
+      C:b 1; d 0,  2;G:p  0, 0;h 1, 3;
+      C:b2;b2100;d0, 1;h 1;
+      G:b0002;d    x,  0;p  0, 0;h 1, 3;
+      C:b 1;b2100;d 0,  2;h  2;    
+      G:b0002;d    x,   4;p   4,  0;p  2, 0;
+      C:b 1;p  2, 0;b 1;p  2, 0;
+      b 1;p  2, 1;F:b3;b312;
+      F:b3;b312;G:p2;p 1;
+      C:b2100;d    x, 1;b 1;C:b2100;
+      Dm:b0;b3322;p 1;p  2;
+      Am:b212;d    x,  2;m  2;b212;
+      C:b 1;d 0,  2;m  0;d   0,   0;
+      
+      C:b 1;d 0,  2;b2100;d    x,  0;
+
+      C:b 1;h  2;G:b 0;d   4,   4;
+      Am:b  2;d  0,   5;C:b  0;d   0,   0;
+      Dm:b   2;s   2,   4;F:b   5;p   5,  0;
+      Am:b  2;h 1;G:b0002;d    x,  0;
+      C:b2;d0, 1;G:b0;d 0,  0;
+      Am:b 1;d 0,  2;Em:b 0;d   4,   4;
+      F:b  2;d  0,   5;C:b  0;d   0,   0;
+      C:b 1; d 0,  2;G:p  0, 0;h 1, 3;
+      C:b2;b2100;d0, 1;h 1;
+      G:b0002;d    x,  0;p  0, 0;h 1, 3;
+      C:b 1;b2100;d 0,  2;h  2;    
+      G:b0002;d    x,   4;p   4,  0;p  2, 0;
+      C:b 1;p  2, 0;b 1;p  2, 0;
+      b 1;p  2, 1;F:b3;b312;
+      F:b3;b312;G:p2;p 1;
+      C:b2100;d    x, 1;b 1;C:b2100;
+      Dm:b0;b3322;p 1;p  2;
+      Am:b212;d    x,  2;m  2;b212;
+      C:b 1;d 0,  2;m  0;d   0,   0;
 
       m  0;;m  2;m 0;
       m 1;
@@ -80,6 +136,7 @@ const Anthems = () => (
   <>
     <RussianShort />
     <RussianFull />
+    <RussianFullWithChords />
   </>
 );
 
