e8de4ae7a46d8f03d23c798df8803cb2bcdcf8e9
Pavel <pfedulov84@gmail.com>
2020-07-01 17:59:42 +0100

    Music notes verse 2

--- src/tunes/MoonlightShadow.tsx
+++ src/tunes/MoonlightShadow.tsx
@@ -3,6 +3,26 @@ import { SheetTemplate } from "../components/Sheet";
 
 // https://musescore.com/user/28594606/scores/5218741
 export const MelodyNotes = () => {
+  const intro = `
+    ;;;h   2,   4;
+  `;
+  const verse1 = `
+    Em:m  0;m  0;h  2;d  0,   4;
+    C:m   2;;m   2;;
+    D:d   4,   4;h  2;;h  2;
+    G:m 0;m 0;D:d  2,   0;d    x,   0;
+  `;
+  const verse2 = `
+    G:m 0;h 3;p 2;d 0,  0;
+    D:d  2,  2;d 0,  2;;;
+    Em:m 0;d x,  2;C:m  0;d  0,   2;
+    D:d  2,  2;d 0,  2;;h  2;
+    G:m 0;h 3;p 2;d 0,  0;
+    D:d  2,   4;p   2;;h   2,   4;
+    Em:d  0,  0;m   4;C:h   2,   4;m  0;
+    D:m  2;
+  `;
+
   return (
     <SheetTemplate
       title="Moonlight Shadow"
@@ -11,14 +31,11 @@ export const MelodyNotes = () => {
       barsPerStave={5}
       meter={4}
       notes={`
-        ;;;h   2,   4;
-        
-        Em:b  0;b  0;h  2;d  0,   4;
-        C:m   2;b2102;m   2;b2102;
-        D:d   4,   4;h  2;d  x,    0;h  2;
-        G:b 0;b 0;D:d  2,   0;d    x,   0;
-        
-    `}
+        ${intro}
+        ${verse1}
+        ${verse1} 
+        ${verse2}       
+      `}
     />
   );
 };
