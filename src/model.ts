3010d562c3ba9572cc08026186cc66cccf7025c2
Pavel <pfedulov84@gmail.com>
2020-07-18 19:22:03 +0100

    Defined new generic model for notes and scores

---
+++ src/model.ts
@@ -0,0 +1,42 @@
+export type Duration = 1 | 2 | 4 | 8 | 16;
+
+export type Note = {
+  string: string;
+  fret: number;
+  duration: Duration;
+  extended?: boolean;
+  chord?: string;
+};
+
+export type Beam = Note[];
+
+export type Bar = {
+  notes: (Note | Beam)[];
+  comment?: string;
+};
+
+export type Part =
+  | {
+      type: "section";
+      title?: string;
+      content: (Bar | Part)[];
+    }
+  | {
+      type: "repeat";
+      title?: string;
+      content: (Bar | Part)[];
+      firstEnding?: Bar[];
+      secondEnding?: Bar[];
+    };
+
+export type Meter = {
+  beatsPerBar: number;
+  beatDuration: Duration;
+};
+
+export type Score = {
+  title: string;
+  tuning: string;
+  meter: Meter;
+  content: (Part | Bar)[];
+};
