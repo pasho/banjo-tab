c5f07968d29ade2168ed9b5cfab43a07eb23b27a
Pavel <pfedulov84@gmail.com>
2020-07-24 07:00:30 +0100

    Chords in section 1

--- src/tunes/PrinceIgor.tsx
+++ src/tunes/PrinceIgor.tsx
@@ -127,10 +127,137 @@ export const MelodyNotes = () => {
   );
 };
 
+export const Clawhammer = () => {
+  return (
+    <SheetTemplate
+      title="Price Igor"
+      description="clawhammer arrangement"
+      tuning="gDGBd"
+      barsPerStave={16}
+      meter={2}
+      notes={`
+       Am:m  2;m  2;
+       m2;b2122;
+       m2;h2
+       m 1;d 0,  2;
+       h 1;m0;
+       G:b0000;m0;
+       b0000;m2;
+       m 0;p  2;
+       Am:m   2;m   2;
+       m  2;b2122;
+       m  2;m 0;
+       m  2;d  0,   4;
+       d  0,   4;m   2;
+       Em:b2002;m   2;
+       b2002;m   4;
+       m  0;m 0;
+       Am:m  2;m  2;
+       m2;b2122;
+       m2;h2;
+       m 1;d 0,  2;
+       h 1;m0;
+       G:b0000;m0;
+       b0000;m2;
+       m 0;p  2;
+       Am:m   2;m   2;
+       m  2;b2122;
+       m  2;m 0;
+       m  2;d  0,   4;
+       d  0,   4;m   2;
+       Em:b2002;m   2;
+       b2002;m   2;
+       b2002;m   2;
+
+       m  2;m  2;
+       m 1;m x;
+       m x;m0;
+       m 1;d 0,  2;
+       h 1;m0;
+       mx;mx;
+       mx;m1;
+       m0;m 0;
+       m1;m1;
+       m5;;
+       m7;;
+       m5;p3,1;
+       d0, 1;m 0;
+       m x;m x;
+       m x;m 1;
+       m0;p 1;
+       m  2;m  2;
+       m 1;m x;
+       m x;m0;
+       m 1;d 0,  2;
+       h  2;m 0;
+       m x;m x;
+       m x;m 1;
+       m 0;m  0;
+       m  2;m  2;
+       m2;mx;
+       mx;h2;
+       m 1;d 0,  2;
+       m 0;m x;
+       m x;m x;
+       m x;m x;
+       ;;  
+
+       m  2;m  2;
+       m2;mx;
+       mx;h2;
+       m 1;d 0,  2;
+       h 1;m0;
+       mx;mx;
+       ;m2;
+       m 0;p  2;
+       m   2;m   2;
+       m  2;m  x;
+       m  x;m 0;
+       m  2;d  0,   4;
+       d  0,   4;m   2;
+       m   x;m   x;
+       ;m   4;
+       m  0;m 0;
+       m  2;m  2;
+       m2;mx;
+       mx;h2;
+       m 1;d 0,  2;
+       h 1;m0;
+       mx;mx;
+       ;h2;
+       m0;p 1;
+       m   2;m   2;
+       m  2;m  x;
+       ;d  2, 0;
+       m  2;d  0,   4;
+       d  0,   4;m   2;
+       m   x;m   x;
+       ;;
+       m  2;m  2;
+       m2;mx;
+       mx;h2;
+       m 1;d 0,  2;
+       d 0,  2;m  0;
+       m  x;;
+       m  2;m  2;
+       m2;mx;
+       mx;h2;
+       m 1;d 0,  2;
+       m 0;m x;
+       m x;;       
+
+
+      
+    `}
+    />
+  );
+};
+
 const PriceIgor = () => (
   <>
-    <MelodyNotes />
     <img src={`${process.env.PUBLIC_URL}/uletai-syao.jpg`} alt="Notes" />
+    <MelodyNotes />
+    <Clawhammer />
   </>
 );
 
