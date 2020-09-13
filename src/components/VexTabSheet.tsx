d9fcb3128ff52c54667cbd50d7e2d3616f54b660
Pasho <pfedulov84@gmail.com>
2020-09-13 19:45:49 +0100

    VexTab adoption (#7)
    * Got vextab working
    
    * Switched to vextab branch
    
    * Complete Lamabada example with vextab
    
    * Corrections
    
    * text tweaks
    
    * Converted lambada to Vex
    
    * Korobeiniki converted to vextab
    
    * converted Korobeiniki

---
+++ src/components/VexTabSheet.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useRef } from "react";
+import { Vex, Artist, VexTab } from "vextab";
+import { useSettings } from "./SettingsContext";
+
+const Renderer = Vex.Flow.Renderer;
+const artist = new Artist(10, 10, 700, { scale: 0.8 });
+const tab = new VexTab(artist);
+
+const tabStaveTemplate = (
+  space: number,
+  showNotes: boolean,
+  tuning: string,
+  time: string,
+  content: string
+) => `
+options space=${space}
+tabstave time=${time} notation=${showNotes} strings=5 tuning=${tuning}
+${content}`;
+
+const VexTabSheet = ({
+  staves,
+  space,
+  tuning,
+  time,
+}: {
+  staves: string[];
+  space?: number;
+  tuning?: string;
+  time?: string;
+}) => {
+  const { showNotes } = useSettings();
+  const vexElement = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    const element = vexElement.current!;
+    element.innerHTML = "";
+    const renderer = new Renderer(element, Renderer.Backends.SVG);
+
+    const input = `
+      options tab-stems=true tab-stem-direction=down scale=0.75 width=1200
+      ${staves
+        .map((stave) =>
+          tabStaveTemplate(
+            space ?? 60,
+            showNotes ?? false,
+            tuning ?? "D/5,B/4,G/4,D/4,G/5",
+            time ?? "4/4",
+            stave
+          )
+        )
+        .join("\n")}
+    `;
+
+    tab.reset();
+    artist.reset();
+    tab.parse(input);
+    artist.render(renderer);
+  }, [staves, tuning, showNotes, space, time]);
+  return <div ref={vexElement}></div>;
+};
+
+export default VexTabSheet;
--- src/tunes/Lambada.tsx
+++ src/tunes/Lambada.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { SheetTemplate } from "../components/Sheet";
+import VexTabSheet from "../components/VexTabSheet";
 
 export const MelodyNotes = () => {
   return (
@@ -59,12 +60,36 @@ export const Clawhammer = () => {
   );
 };
 
-const Lambada = () => (
-  <>
-    <MelodyNotes />
-    <Clawhammer />
-    <img src={`${process.env.PUBLIC_URL}/lambada.png`} alt="Notes" />
-  </>
-);
+const Lambada = () => {
+  return (
+    <>
+      <h1>Lambada</h1>
+      <p>gDGBd</p>
+      <VexTabSheet
+        {...{
+          staves: [
+            `
+            notes :q 2/4
+            notes =|: :8 2/1-0/5 2p0/1 1p0/2 :q 2/3 |
+            notes :8 2/3-1/2 0/1-2/3 0h2/3 2p0/4 |
+            notes :q 2/4 :8 (2/1.1/2.0/3.2/4)-0/5 0h2/4 (2/1.1/2.0/3.2/4)-0/5 =:|
+            text .-1,:q,Part A,|,:w, ,|, ,|, ,|
+            text ++,.0,:q,Am,|,:w, ,|,:h,F,G,|,:w,C,|
+          `,
+            `
+            notes =|: :8 0/1-0/5 0/1-1/2 :q 3/4 :8 2/3-1/2 |
+            notes :8 2/1-0/5 0/1-1/2 :q 3/4 :8 2/3-1/2 |
+            notes :8 0/1-0/5 2p0/3 0-0/3 2p0/3 |
+            notes :q 2/3 :8 (2/1.1/2.2/3.2/4)-0/5 0h2/3 (2/1.1/2.2/3.2/4)-0/5 |
+            notes :8 1p0/2 :h 2v/3 =:|
+            text .-1,|Part B,:w, ,|, ,|, ,|1., ,|2.,:hd, ,|
+            text ++,.0,|,:w,Dm,|, ,|,G,|,Am,|,:hd,Am,|
+          `,
+          ],
+        }}
+      />
+    </>
+  );
+};
 
 export default Lambada;
