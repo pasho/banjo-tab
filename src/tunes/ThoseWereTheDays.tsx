0aecea95580940838b67b25ea4c767b0f7c7a851
Pasho <pfedulov84@gmail.com>
2020-10-21 11:25:44 +0100

    Cleanup

--- src/tunes/ThoseWereTheDays.tsx
+++ src/tunes/ThoseWereTheDays.tsx
@@ -5,31 +5,20 @@ import VexTabSheet from "../components/VexTabSheet";
 export default function ThoseWereTheDays() {
   return (
     <>
-      <h1>Those were the days (Dorogoy Dlinnoyu) V1</h1>
+      <h1>Those were the days (Dorogoy Dlinnoyu)</h1>
+      <p>Clawhammer Banjo arrangement</p>
       <p>f#DGBd</p>
       <VexTabSheet
         {...{
           time: "4/4",
           tuning: "D/5,B/4,G/4,D/4,F#/5",
           staves: [
-            // `
-            //   notes :8 0/2-0/2 $.top.II, $ 2h3/2 2p0/1 2p0/2 |
-            //   notes :8 0/1-0/2 $.top.Bm, $ t:hd:0/2 |
-            //   notes :8 4/4-4/4 0h2/3 0/2-2/3 0/3-4/4 |
-            //   notes :w 2/4 $.top.Em$ |
-            // `,
             `
               notes :8 0/2-4/3 $.top.II, $ 2h3/2 2p0/1 2p0/2 |
               notes :8 0/1-0/2 $.top.Bm, $ (0/1.0/2.4/3.4/4)-0/5 :q 0/2 :8 (0/1.0/2.4/3.4/4)-0/5 |
               notes :8 4/4-4/4 0h2/3 0/2-2/3 0/3-4/4 | 
               notes :q 2/4 $.top.Em$ :8 (2/1.0/2.0/3.2/4)-0/5 :q 2/4 :8 (2/1.0/2.0/3.2/4)-0/5 |
             `,
-            // `
-            //   notes :8 0/2-0/2 2h3/2 2p0/1 2p0/2 |
-            //   notes :8 0/1-0/2 $.top.Bm, $ t:hd:0/2 |
-            //   notes :8 2/2-2/2 $.top.I, $  2h3/2 2/2-1/3 3s4/3 |
-            //   notes :w 2/2 $.top.F#7/F#m7$ |
-            // `,
             `
               notes :8 0/2-0/2 2h3/2 2p0/1 2p0/2 |
               notes :8 0/1-0/2 $.top.Bm, $ (0/1.0/2.4/3.4/4)-0/5 :q 0/2 :8 (0/1.0/2.4/3.4/4)-0/5 |
@@ -42,13 +31,6 @@ export default function ThoseWereTheDays() {
               notes :q 2/3 :8 (0/1.0/2.0/3.0/4)-5/4 $.top.G, $ ## 2/4 4s5/4 |
               notes :q 2/3 :8 (2/1.2/2.2/3.2/4)h4/3 $.top.A, $ :8d 2p:16:0/2 :8 2p0/3 |
             `,
-            // `
-            //   notes :h 4/4 :8 ## 0/4 2h4/4 |
-            //   notes :q 2/3 :8 0/3-0/3 ## 0/3 2h4/3 |
-            //   notes :q 2/1 :8 0/1-0/1 ## 0/2 2h3/2 |
-            //   notes :qd 4/1 :8 2/1 : 8d 0/1-:16:2/2 :8 0/2-3/3 |
-            //   notes :w 0/2 =|=
-            // `,
             `
               notes :h 4/4 :8 (4/1.3/2.2/3.4/4)p0/4 2h4/4 |
               notes :q 2/3 :8 (0/1.0/2.0/3.0/4)-5/4 $.top.G, $ ## 0/3 2h4/3 |
@@ -59,7 +41,7 @@ export default function ThoseWereTheDays() {
           ],
         }}
       />
-      <h1>Those were the days (Dorogoy Dlinnoyu) V2</h1>
+      {/* <h1>Those were the days (Dorogoy Dlinnoyu) V2</h1>
       <p>gDGBd</p>
       <VexTabSheet
         {...{
@@ -92,7 +74,7 @@ export default function ThoseWereTheDays() {
             `,
           ],
         }}
-      />
+      /> */}
     </>
   );
 }
