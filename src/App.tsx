1c3b4c953829811992d777ef3902817bae24f92b
Pavel <pfedulov84@gmail.com>
2021-09-03 22:47:42 +0100

    Added nice topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import React, { useState, useEffect } from "react";
 import { Switch, Route, Redirect, HashRouter, Link } from "react-router-dom";
+import {
+  AppBar,
+  Switch as Toggle,
+  createStyles,
+  IconButton,
+  makeStyles,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+  FormControlLabel,
+  FormGroup,
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
 
 import SettingsContext from "./components/SettingsContext";
 import tunes from "./tunes";
@@ -22,31 +36,81 @@ const TitleRoute: React.FunctionComponent<TitleRouteProps> = ({
   return <Route {...{ path }}>{children}</Route>;
 };
 
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  })
+);
+
 const App = () => {
   const [showNotes, setShowNotes] = useState(false);
+  const classes = useStyles();
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <HashRouter>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+              onClick={handleClick}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="simple-menu"
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              {tunes.map((tune) => (
+                <MenuItem onClick={handleClose} component={Link} to={tune.path}>
+                  {tune.title}
+                </MenuItem>
+              ))}
+            </Menu>
+            <Typography variant="h6" className={classes.title}>
+              Banjo Tabs
+            </Typography>
+            <FormGroup row>
+              <FormControlLabel
+                control={
+                  <Toggle
+                    checked={showNotes}
+                    onChange={(e) => setShowNotes(e.target.checked)}
+                    inputProps={{ "aria-label": "primary checkbox" }}
+                  />
+                }
+                label="Show Notes"
+              />
+            </FormGroup>
+          </Toolbar>
+        </AppBar>
+      </div>
       <div className="App">
         <div className="content">
-          <span>
-            <input
-              type="checkbox"
-              id="showNotes"
-              checked={showNotes}
-              onChange={() => setShowNotes(!showNotes)}
-            />
-            <label htmlFor="showNotes">Show Notes</label>
-            <br />
-            {tunes.map((tune, i) => {
-              const isLast = i + 1 === tunes.length;
-              return (
-                <>
-                  <Link to={tune.path}>{tune.title}</Link>
-                  {!isLast ? "::" : ""}
-                </>
-              );
-            })}
-          </span>
           <SettingsContext {...{ showNotes }}>
             <Switch>
               {tunes.map((tune) => {
